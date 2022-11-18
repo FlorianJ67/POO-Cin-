006cb3d9157f7e917ecbda87481726bd4485ed4b
Florian JACOB <florian.jacob96@gmail.com>
2022-11-18 15:14:13 +0100

    first commit

---
+++ Film.php
@@ -0,0 +1,38 @@
+<?php
+class Film {
+    protected $_title;
+    protected $_launchDate;
+    protected $_length;
+    protected $_realisator;
+        
+    public function __construct(string $title,$launchDate,$length, Realisator $realisator){
+        $this->_title = $title;
+        $this->_launchDate = $launchDate;
+        $this->_length = $lentgh;
+        $this->_realisator= $realisator;
+        $this->_realisator->addFilm($this);
+    }
+            
+    //GET
+    public function getTitle(){
+        return $this->_title;
+    }
+    public function getLaunchDate(){
+        return $this->_launchDate;
+    }
+    public function getLength(){
+        return $this->_length;
+    }
+        
+    //SET
+    public function settitle($title){
+        $this->_libelle = $libelle;
+    }
+    public function setLaunchDate($LaunchDate){
+        $this->_devise = $devise;
+    }
+    public function setLength($length){
+        $this->_length = $length;
+    }
+                        
+}
---
+++ Realisator.php
@@ -0,0 +1,52 @@
+<?php
+class Realisator {
+    protected $_name;
+    protected $_fName;
+    protected $_sexe;
+    protected $_birthday;
+    protected $_films;
+        
+    public function __construct(string $fName,string $name,string $sexe,date $birthday){
+        $this->_name = $name;
+        $this->_fName = $fName;
+        $this->_sexe = $sexe;
+        $this->_birthday = $birthday;
+        $this->_films = [];
+    }
+            
+    //GET
+    public function getName(){
+        return $this->_name;
+    }
+    public function getFName(){
+        return $this->_fName;
+    }
+    public function getsexe(){
+        return $this->_sexe;
+    }
+    public function getBirthday(){
+        return $this->_birthday;
+    }
+
+        
+    //SET
+    public function setName($name){
+        $this->_name = $name;
+    }
+    public function setFName($fName){
+        $this->_fName = $fName;
+    }
+    public function setSexe($sexe){
+        $this->_sexe = $sexe;
+    }
+    public function setBirthday($birthday){
+        $this->_birthday = $birthday;
+    }
+    
+
+    
+    public function addFilm($newFilm){
+        array_push($this->_films,$newFilm);
+    }
+}
+?>
